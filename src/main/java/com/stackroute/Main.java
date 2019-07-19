21cb7791ec88e4fde037ff724510c37aa74d0b20
HARIKABODDU11 <51943015+HARIKABODDU11@users.noreply.github.com>
2019-07-19 12:15:44 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -9,8 +9,19 @@ public class Main {
 
 
     public  static void main(String[]args){
+        //intialize application  context with configuration class
         ApplicationContext context=new  AnnotationConfigApplicationContext(Config.class);
         Movie movie=context.getBean("movie",Movie.class);
         movie.displayActor();
+        
+        /initialize BeanFactory with configuration class
+       BeanFactory beanFactory = new ConfigurableBeanFactory(new ClassPathResource(Config.class));
+       Movie movie1 = beanFactory.getBean("movie",Movie.class);
+       movie1.displayActorDetails();
+       //initialize BeanFactory with configuration class
+       DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+       BeanDefinitionRegistry beanDefinitionRegistry = new GenericApplicationContext(factory);
+       BeanDefinitionReader reader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
+       reader.loadBeanDefinitions(Config.class);
     }
 }
